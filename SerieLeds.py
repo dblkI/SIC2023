e23b095e69726d489463ce17644fd6198f96098b
RaspberryT3 <dblanquelg1500@alumno.ipn.mx>
2023-10-20 19:37:51 -0600

    ledsgpios

--- SerieLeds.py
+++ SerieLeds.py
@@ -1,8 +1,8 @@
 from gpiozero import LED
 import time
 
-# Configura los LEDs en los pines GPIO 17, 18, 22 y 23
-leds = [LED(17), LED(18), LED(22), LED(23)]
+# Configura los LEDs en los pines GPIO 2, 3, 4 y 8
+leds = [LED(2), LED(3), LED(4), LED(8)]
 
 # Función para encender los LEDs según la secuencia del archivo
 def encender_leds(secuencia):
